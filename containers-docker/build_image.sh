b5c264070b5b7c7cfb72987259d5f2e9ffd9ac22
uareurapid <cristo.paulo@gmail.com>
2023-10-06 16:58:51 +0100

    wip: update this

--- containers-docker/build_image.sh
+++ containers-docker/build_image.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker build . -t poc-large-files/backend
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied! Usage: $0 <image-tag-name>"
+    exit
+fi
+image_tag=$1
+
+docker build . -t $image_tag
