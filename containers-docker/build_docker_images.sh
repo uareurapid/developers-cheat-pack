beb1258f0d078ca0a794380aafbaa116c010500f
uareurapid <cristo.paulo@gmail.com>
2023-10-14 19:57:50 +0100

    update docker scripts

--- containers-docker/build_docker_images.sh
+++ containers-docker/build_docker_images.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+#use this script to bootstrap a docker-compose configuration
+#builds the images and starts the containers
 docker-compose up --build
--- containers-docker/run_image.sh
+++ containers-docker/run_image.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+if [ ! $# -eq 2 ]
+  then
+    echo "No arguments supplied! Usage: $0 <port:port> <image-tag-name>"
+    exit
+fi
+
+#for simplicity local and container ports are the same
+ports=$1
+image_tag=$2
+
 #port and image name
-docker run -p 3000:3000 -d poc-large-files/backend
+docker run -p $ports -d $image_tag
